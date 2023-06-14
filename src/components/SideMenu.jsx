9b7157e4c4c0a9a214bea40bc0353fe139b744cd
VladislavTU <timofeevvladislavu@gmail.rom>
2023-06-14 21:04:57 +0400

    side bar in work 0614

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -6,18 +6,39 @@ function SideMenu() {
       <div className={styles.menuLogoContainer}>
         <img className={styles.menuLogo} src="./imgs/logo.svg" alt="logo" />
       </div>
-      <div className={styles.phoneMenuContainer}>
-        <img src="./imgs/phone.svg" alt="phone" />
-        <p className={styles.phoneMenu}>+7 495 902 61 04</p>
+      <div className={styles.sideMenuMiddle}>
+        {' '}
+        <div className={styles.phoneMenuContainer}>
+          <img src="./imgs/phone.svg" alt="phone" />
+          <p className={styles.phoneMenu}>+7 495 902 61 04</p>
+        </div>
+        <div className={styles.sideMenuListContainer}>
+          <p>Проекты домов</p>
+          <p>Услуги</p>
+          <p>О компании</p>
+          <p>Наши работы</p>
+          <p>Отзывы</p>
+          <p>Акции</p>
+          <p>Контакты</p>
+        </div>
       </div>
-      <div className={styles.sideMenuListContainer}>
-        <p>Проекты домов</p>
-        <p>Услуги</p>
-        <p>О компании</p>
-        <p>Наши работы</p>
-        <p>Отзывы</p>
-        <p>Акции</p>
-        <p>Контакты</p>
+      <div className={styles.sideMenuBottom}>
+        <div className={styles.menuFind}>
+          <input type="text" placeholder="Поиск" />
+          <img src="./imgs/find.svg" alt="find" />
+        </div>
+        <div className={styles.sideMenuCallButton}>
+          <p>Заказать звонок</p>
+        </div>
+        <div>
+          <img src="./imgs/instagram.svg" alt="instagram" />
+          <img src="./imgs/vk.svg" alt="vk" />
+          <img src="./imgs/facebook.svg" alt="facebook" />
+        </div>
+        <div>
+          <p>Пользовательское соглашение</p>
+          <p>Политика конфиденциальности</p>
+        </div>
       </div>
     </div>
   );
